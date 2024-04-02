7a9ec5e6c123463bd5e6a295333a1e1c0a06e90e
nomissbowling <nomissbowling@yahoo.co.jp>
2024-04-02 11:26:44 +0900

    version 0.2.8

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/crate/dxlib_sample_0001/0.2.2")]
+#![doc(html_root_url = "https://docs.rs/crate/dxlib_sample_0001/0.2.8")]
 //! sample dxlib dll for Rust
 //!
 //! see also https://docs.rs/dxlib/latest/dxlib/dx
