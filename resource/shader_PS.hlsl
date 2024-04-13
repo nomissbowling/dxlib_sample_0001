833bc222c55cf07844a2676ffce58e3fc3b90d32
nomissbowling <nomissbowling@yahoo.co.jp>
2024-04-13 10:54:41 +0900

    version 0.4.1

--- resource/shader_PS.hlsl
+++ resource/shader_PS.hlsl
@@ -94,8 +94,7 @@ cbuffer cb_7 : register(b7) {
 struct CamLight {
   float4 cam_pos4;
   float4 cam_lat4;
-  float4 r0;
-  float4 r1;
+  float4 r[2];
 };
 
 cbuffer cb_CamLight : register(b8) { // max 14 slots
@@ -105,13 +104,19 @@ cbuffer cb_CamLight : register(b8) { // max 14 slots
 struct LIGHT {
   float4 test;
   float4 amb;
-  float4 vec4;
+  float4 spc;
   float a;
   float3 padding;
 };
 
-LIGHT proc_light(float3 n, float3 lookat, int lh)
+LIGHT proc_light(PS_INPUT psi, int lh)
 {
+  float3 n = normalize(psi.norm.xyz);
+  float3 lookat = g_CL.cam_lat4.xyz - g_CL.cam_pos4.xyz;
+//  float3 lookat = psi.ppos.xyz - g_CL.cam_pos4.xyz;
+//  float3 lookat = psi.pos.xyz - g_CL.cam_pos4.xyz;
+//  float3 lookat = float3(0.0f, 0.0f, 0.0f);
+
   DX_D3D11_CONST_LIGHT light = g_Common.Light[lh];
   float4 light_amb = light.Ambient;
   float4 light_pos4;
@@ -135,95 +140,86 @@ LIGHT proc_light(float3 n, float3 lookat, int lh)
 //  float3 r = normalize(lookat + light_dir);
 //  float3 r = -normalize(lookat + light_dir);
 //  float3 r = -normalize(light_dir);
-  LIGHT l = {test, light_amb, light_vec4, max(0.0f, dot(r, n)), float3(0.0f, 0.0f, 0.0f)};
+  float a = max(0.0f, dot(r, n));
+  float4 spc = psi.spc * pow(a, light_vec4.w);
+//  float4 spc = psi.spc * pow(a, 1.0f);
+  LIGHT l = {test, light_amb, spc, a, float3(0.0f, 0.0f, 0.0f)};
   return l;
 }
 
-float m_a(float r[6], LIGHT l[6], int i)
+LIGHT m_s(CamLight e, LIGHT l[6], int i) // mul struct r LIGHT
 {
-  return max(0.0f, r[i] * l[i].a);
+  float r[8]; // not use loop in HLSL
+  r[0] = e.r[0].x;
+  r[1] = e.r[0].y;
+  r[2] = e.r[0].z;
+  r[3] = e.r[0].w;
+  r[4] = e.r[1].x;
+  r[5] = e.r[1].y;
+  r[6] = e.r[1].z;
+  r[7] = e.r[1].w;
+  LIGHT o = {
+    float4(0.0f, 0.0f, 0.0f, 0.0f),
+    max(0.0f, r[i] * l[i].amb),
+    max(0.0f, r[i] * l[i].spc),
+    max(0.0f, r[i] * l[i].a),
+    float3(0.0f, 0.0f, 0.0f)};
+  return o;
 }
 
-float dot_a(float r[6], LIGHT l[6])
+LIGHT a_s(LIGHT a, LIGHT b) // add struct LIGHT LIGHT
 {
-  return m_a(r, l, 0) + m_a(r, l, 1) + m_a(r, l, 2)
-    + m_a(r, l, 3) + m_a(r, l, 4) + m_a(r, l, 5);
+  LIGHT o = {
+    a.test + b.test,
+    a.amb + b.amb,
+    a.spc + b.spc,
+    a.a + b.a,
+    a.padding + b.padding};
+  return o;
 }
 
-float4 m_amb(float r[6], LIGHT l[6], int i)
+LIGHT c_s(float c, LIGHT a) // clip struct s LIGHT
 {
-  return max(0.0f, r[i] * l[i].amb);
+  LIGHT o = {
+    min(c, a.test),
+    min(c, a.amb),
+    min(c, a.spc),
+    min(c, a.a),
+    min(c, a.padding)};
+  return o;
 }
 
-float4 dot_amb(float r[6], LIGHT l[6])
+LIGHT dot_s(float c, CamLight e, LIGHT l[6]) // dot and clip struct c r LIGHT
 {
-  return m_amb(r, l, 0) + m_amb(r, l, 1) + m_amb(r, l, 2)
-    + m_amb(r, l, 3) + m_amb(r, l, 4) + m_amb(r, l, 5);
-}
-
-float m_s(float r[6], float4 s[6], int i)
-{
-  return max(0.0f, r[i] * s[i]);
-}
-
-float4 dot_s(float r[6], float4 s[6])
-{
-  return m_s(r, s, 0) + m_s(r, s, 1) + m_s(r, s, 2)
-    + m_s(r, s, 3) + m_s(r, s, 4) + m_s(r, s, 5);
+  return c_s(c, a_s(a_s(a_s(a_s(a_s(
+    m_s(e, l, 0), m_s(e, l, 1)), m_s(e, l, 2)),
+    m_s(e, l, 3)), m_s(e, l, 4)), m_s(e, l, 5)));
 }
 
 PS_OUTPUT main(PS_INPUT psi)
 {
   PS_OUTPUT pso;
 
-  float3 norm = psi.norm.xyz;
-  float3 lat = g_CL.cam_lat4.xyz - g_CL.cam_pos4.xyz;
-//  float3 lat = float3(0.0f, 0.0f, 0.0f);
-
   LIGHT l[6]; // not use loop in HLSL
-  l[0] = proc_light(normalize(norm), lat, 0);
-  l[1] = proc_light(normalize(norm), lat, 1);
-  l[2] = proc_light(normalize(norm), lat, 2);
-  l[3] = proc_light(normalize(norm), lat, 3);
-  l[4] = proc_light(normalize(norm), lat, 4);
-  l[5] = proc_light(normalize(norm), lat, 5);
+  l[0] = proc_light(psi, 0);
+  l[1] = proc_light(psi, 1);
+  l[2] = proc_light(psi, 2);
+  l[3] = proc_light(psi, 3);
+  l[4] = proc_light(psi, 4);
+  l[5] = proc_light(psi, 5);
 
-  float4 s[6]; // not use loop in HLSL
-  s[0] = psi.spc * pow(l[0].a, l[0].vec4.w);
-//  s[0] = psi.spc * pow(l[0].a, 1.0f);
-  s[1] = psi.spc * pow(l[1].a, l[1].vec4.w);
-//  s[1] = psi.spc * pow(l[1].a, 1.0f);
-  s[2] = psi.spc * pow(l[2].a, l[2].vec4.w);
-//  s[2] = psi.spc * pow(l[2].a, 1.0f);
-  s[3] = psi.spc * pow(l[3].a, l[3].vec4.w);
-//  s[3] = psi.spc * pow(l[3].a, 1.0f);
-  s[4] = psi.spc * pow(l[4].a, l[4].vec4.w);
-//  s[4] = psi.spc * pow(l[4].a, 1.0f);
-  s[5] = psi.spc * pow(l[5].a, l[5].vec4.w);
-//  s[5] = psi.spc * pow(l[5].a, 1.0f);
-
-  float r[6]; // not use loop in HLSL
-  r[0] = g_CL.r0.x;
-  r[1] = g_CL.r0.y;
-  r[2] = g_CL.r0.z;
-  r[3] = g_CL.r0.w;
-  r[4] = g_CL.r1.x;
-  r[5] = g_CL.r1.y;
-//  float r[6] = {0.8f, 0.8f, 0.8f, 0.8f, 0.0f, 0.0f}; // not use loop in HLSL
-  float a = min(1.0f, dot_a(r, l));
-  float4 amb = min(1.0f, dot_amb(r, l));
-  float4 ss = min(1.0f, dot_s(r, s));
+  LIGHT o = dot_s(1.0f, g_CL, l);
 
   // texture diffused color
   float4 dc = g_DiffuseMapTexture.Sample(g_DiffuseMapSampler, psi.texCoords0);
 //  pso.color0 = dc * psi.dif; // not use light spc
-//  pso.color0 = dc * psi.dif * l[0].a + s[0] + l[0].amb; // only light 0
-//  pso.color0 = dc * psi.dif * l[1].a + s[1] + l[1].amb; // only light 1
-//  pso.color0 = (dc * psi.dif * l[1].a + s[1] + l[1].amb)
-//    * l[0].a + s[0] + l[0].amb;
-  pso.color0 = dc * psi.dif * a + ss + amb;
-//  pso.color0 = test0; // test by light 0 direction or specular
-//  pso.color0 = test1; // test by light 1 direction or specular
+//  pso.color0 = dc * psi.dif * l[0].a + l[0].spc + l[0].amb; // only light 0
+//  pso.color0 = dc * psi.dif * l[1].a + l[1].spc + l[1].amb; // only light 1
+//  pso.color0 = (dc * psi.dif * l[1].a + l[1].spc + l[1].amb)
+//    * l[0].a + l[0].spc + l[0].amb;
+  pso.color0 = dc * psi.dif * o.a + o.spc + o.amb;
+//  pso.color0 = l[0].test; // test by light 0 direction or specular
+//  pso.color0 = l[1].test; // test by light 1 direction or specular
 //  pso.color0 = g_CL.cam_pos4; // test by constant buffer
 //  pso.color0 = float4(r[0], r[1], r[2], r[3]); // test by constant buffer
   return pso;
